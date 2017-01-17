29146fa3acdc387f6804201e104d170c69c15400
eve peralta <eperalta.cm@gmail.com>
2017-01-17 14:46:13 -0300

    validacion formulario crear materiales

--- js/main.js
+++ js/main.js
@@ -24,4 +24,64 @@ $(document).ready(function(){
      }
     });
 
+
+   $("button").on("click", function(e){
+	
+			if(!validateForm()){
+				console.log("go fish")
+				e.preventDefault();
+			}
+		});
+function validateForm(){
+
+	if(validarCampoTexto($("#producto")) == false){
+		return false
+	}
+	if(validarTextArea1($("#textarea1"))== false){
+		return false
+	}
+	if(validarTextArea2($("#textarea2"))== false){
+		return false
+	}
+	return true;
+}
+
+function validarCampoTexto(input){
+	var bool = true;
+
+	if($("#producto").val() == ""){
+		$("#producto").attr('placeholder', "Este campo es obligatorio");
+		bool = false
+	}else{
+		console.log("ok")
+		}
+		return bool
+	};
+	
+
+function validarTextArea1(textarea){
+	var bool = true;
+
+	if($("#textarea1").val() == ""){
+		$("#textarea1").attr('placeholder', "Campo obligatorio");
+		bool = false
+	}else{
+		console.log("ok")
+	}
+	return bool
+};
+function validarTextArea2(textarea){
+	var bool = true;
+
+	if($("#textarea2").val() == ""){
+		$("#textarea2").attr('placeholder', "Campo obligatorio");
+		bool = false
+	}else{
+		console.log("ok")
+	}
+	return bool
+};
+
+
 });
+
